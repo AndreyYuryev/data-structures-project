0026fd6146e8015941caa0c25522cb0b5b3c8f04
Andrey <yuryev@yandex.ru>
2023-10-20 16:26:20 +0300

    + linkedlist

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,43 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data, next_node):
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        ''' конструктор '''
+        self.head = None
+
     def insert_beginning(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        """Принимает данные (словарь) и добавляет узел с
+        этими данными в начало связанного списка"""
+        new_node = Node(data, None)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next_node = self.head
+            self.head = new_node
 
     def insert_at_end(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        """Принимает данные (словарь) и добавляет узел с
+        этими данными в конец связанного списка"""
+        new_node = Node(data, None)
+        if self.head is None:
+            self.head = new_node
+        elif self.head.next_node is None:
+            self.head.next_node = new_node
+        else:
+            node_from = self.head
+            next_node = node_from.next_node
+            while next_node is not None:
+                node_from = next_node
+                next_node = node_from.next_node
+            node_from.next_node = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -22,8 +47,8 @@ class LinkedList:
 
         ll_string = ''
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f'{str(node.data)} -> '
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += 'None'
         return ll_string
